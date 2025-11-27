d15ed2c4ef738666be6092b6cc0156b66659e005
Maria Vanegas <marisavaneg@hotmail.com>
2025-11-26 22:09:59 -0800

    Banking Project
    Working on page structure and some functionality

--- Projects/NetBanking Simulator/index.js
+++ Projects/NetBanking Simulator/index.js
@@ -7,3 +7,19 @@
             - Transfer Out (Debit)
 
 */
+
+const accountNumber = "102230379866";
+const eyeBtn = document.getElementById("see-account-number");
+const accountDigits = document.getElementById("account-digits");
+let accountNumberVisible = false;
+eyeBtn.addEventListener("click", () => {
+  eyeBtn.classList.toggle("fa-eye");
+  eyeBtn.classList.toggle("fa-eye-slash");
+  accountNumberVisible = !accountNumberVisible;
+
+  if (accountNumberVisible) {
+    accountDigits.innerText = accountNumber;
+  } else {
+    accountNoText.innerText = `*********${accountNumber.slice(-3)}`;
+  }
+});
