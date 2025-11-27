6574683ef160d010a8f6c26496b950cef68b3b12
Maria Vanegas <marisavaneg@hotmail.com>
2025-11-26 17:35:19 -0800

    Banking Project

---
+++ Projects/NetBanking Simulator/index.js
@@ -0,0 +1,9 @@
+/*
+    # Requirements:
+        - Account balance
+        - Account statement (Transactions)
+        - Transfer
+            - Transfer In (Credit)
+            - Transfer Out (Debit)
+
+*/
