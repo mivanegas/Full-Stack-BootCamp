45b734eba4bde21f65232a1b8dc0809a5bf6147d
Maria Vanegas <marisavaneg@hotmail.com>
2025-11-28 16:58:44 -0800

    Bank Project

--- Projects/NetBanking Simulator/index.js
+++ Projects/NetBanking Simulator/index.js
@@ -1,32 +1,22 @@
-/*
-    # Requirements:
-        - Account balance
-        - Account statement (Transactions)
-        - Transfer
-            - Transfer In (Credit)
-            - Transfer Out (Debit)
-
-*/
-
 //Making account number visible/invisible
 const accountNumber = "102230379866";
 const eyeBtn = document.getElementById("see-account-number");
-const accountDigits = document.getElementById("account-digits");
-let accountNumberVisible = false;
+const accountNoText = document.getElementById("account-no-text");
+let accountNoVisible = false;
 eyeBtn.addEventListener("click", () => {
   eyeBtn.classList.toggle("fa-eye");
   eyeBtn.classList.toggle("fa-eye-slash");
-  accountNumberVisible = !accountNumberVisible;
+  accountNoVisible = !accountNoVisible;
 
-  if (accountNumberVisible) {
-    accountDigits.innerText = accountNumber;
+  if (accountNoVisible) {
+    accountNoText.innerText = accountNumber;
   } else {
     accountNoText.innerText = `*********${accountNumber.slice(-3)}`;
   }
 });
 
 // Adding transactions to Account Statement
-const transactionList = document.getElementById("transction-list");
+const transactionList = document.getElementById("transaction-list");
 
 /*
  Replicate <a> and <div> using JS (below)
@@ -43,24 +33,102 @@ const transactionList = document.getElementById("transction-list");
          </a>
 */
 
-function addToTransaction() {
+function deposit() {
+  // Collect form data and reset
+  const amountIn = document.getElementById("amount-to-deposit");
+  const amountToDeposit = Number(amountIn.value);
+  amountIn.value = "";
+
+  //Creating element structure using DOM Methods
   const listGroupItem = document.createElement("a");
   listGroupItem.classList.add("list-group-item", "list-group-item-action");
-  listGroupItem.href = "#";
 
   const topPart = document.createElement("div");
   topPart.classList.add("d-flex", "w-100", "justify-content-between");
 
   const h5 = document.createElement("h5");
   h5.classList.add("mb-1");
-  h5.innerText = "Deposit Transfer";
+  h5.innerText = "Deposit";
 
   const amount = document.createElement("div");
-  amount.classList.add("fs-5,", "text-danger");
-  amount.innerText = "$12,000.00";
+  amount.classList.add("fs-5", "text-success");
+  amount.innerText = `+ $${amountToDeposit.toFixed(2)}`;
 
   topPart.append(h5, amount);
 
-  listGroupItem.append(topPart);
+  const middlePart = document.createElement("p");
+  middlePart.classList.add("mb-1");
+  middlePart.innerText = "Transfer in";
+
+  const lastPart = document.createElement("div");
+  lastPart.classList.add("d-flex", "w-100", "justify-content-between");
+
+  const small1 = document.createElement("small");
+  small1.innerText = "11/28/2025 1:28 PM";
+  const small2 = document.createElement("small");
+  small2.innerText = "Balance: $178,000.75";
+  lastPart.append(small1, small2);
+
+  listGroupItem.append(topPart, middlePart, lastPart);
   transactionList.append(listGroupItem);
+
+  // Show alert
+  document.getElementById("deposit-successful").classList.add("show");
 }
+
+function sendMoney() {
+  // Collect form data and reset
+  const amountToTransfer = Number(
+    document.getElementById("amount-to-transfer").value
+  );
+  const transferType = document.getElementById("transfer-type").value;
+  const beneficiaryName = document.getElementById("beneficiary-name").value;
+  const sendMoneyForm = document.getElementById("send-money-form");
+  sendMoneyForm.reset();
+
+  //Creating element structure using DOM Methods
+  const listGroupItem = document.createElement("a");
+  listGroupItem.classList.add("list-group-item", "list-group-item-action");
+
+  const topPart = document.createElement("div");
+  topPart.classList.add("d-flex", "w-100", "justify-content-between");
+
+  const h5 = document.createElement("h5");
+  h5.classList.add("mb-1");
+  h5.innerText = `${transferType} Transfer: ${beneficiaryName}`;
+
+  const amount = document.createElement("div");
+  amount.classList.add("fs-5", "text-success");
+  amount.innerText = `- $${amountToDeposit.toFixed(2)}`;
+
+  topPart.append(h5, amount);
+
+  const middlePart = document.createElement("p");
+  middlePart.classList.add("mb-1");
+  middlePart.innerText = "Transfer out";
+
+  const lastPart = document.createElement("div");
+  lastPart.classList.add("d-flex", "w-100", "justify-content-between");
+
+  const small1 = document.createElement("small");
+  small1.innerText = "11/28/2025 1:28 PM";
+  const small2 = document.createElement("small");
+  small2.innerText = "Balance: $178,000.75";
+  lastPart.append(small1, small2);
+
+  listGroupItem.append(topPart, middlePart, lastPart);
+  transactionList.append(listGroupItem);
+
+  // Show alert
+  document.getElementById("transfer-successful").classList.add("show");
+}
+
+/*
+    # Requirements:
+        - Account balance
+        - Account statement (Transactions)
+        - Transfer
+            - Transfer In (Credit)
+            - Transfer Out (Debit)
+
+*/
